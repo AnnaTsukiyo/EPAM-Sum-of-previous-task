fb8d66e3f691f402643098bdb44a521d457cc01e
AnnaTsukiyo <annett.sav4enko@gmail.com>
2022-01-25 18:02:58 +0200

    done

--- src/main/java/com/epam/rd/autotasks/arrays/SumOfPrevious.java
+++ src/main/java/com/epam/rd/autotasks/arrays/SumOfPrevious.java
@@ -6,14 +6,24 @@ public class SumOfPrevious {
 
     public static void main(String[] args) {
         int[] array = new int[]{1, -1, 0, 4, 6, 10, 15, 25};
-
         System.out.println(Arrays.toString(getSumCheckArray(array)));
     }
 
-    public static boolean[] getSumCheckArray(int[] array){
-
-        //put your code here
-
-        throw new UnsupportedOperationException();
+    public static boolean[] getSumCheckArray(int[] array) {
+//        if (array == null || array.length < 2) {
+//            System.out.println();
+//        }
+        boolean[] booleans = new boolean[array.length];
+        booleans[0] = false;
+        booleans[1] = false;
+        for (int i = 2; i < array.length; i++) {
+            if (array[i] == (array[i - 1] + array[i - 2])) {
+                booleans[i] = true;
+                Arrays.copyOf(array, i);
+            } else {
+                booleans[i] = false;
+            }
+        }
+        return booleans;
     }
 }
